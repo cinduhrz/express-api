70a884ebd0071a5b6395891af592343d5ae44db8
Cindy Wong <cindywongdev@gmail.com>
2023-01-04 19:25:25 -0500

    finished create route

--- server.js
+++ server.js
@@ -5,10 +5,38 @@ const express = require("express")
 // create express App object
 const app = express()
 
+// The Data
+const turtles = [
+    {name: "Leonardo", role: "ninja"},
+    {name: "Michaelangelo", role: "ninja"},
+    {name: "Donatello", role: "ninja"},
+    {name: "Raphael", role: "ninja"},
+]
 
-// Routes
+// middleware
+app.use(express.json()) // req.body json parser (need for post requests to accept json data from client)
+
+
+// register routes
 app.get('/', (req, res) => {
-    res.json({ response: "hello world" })
+    res.json({
+        response: "hello world"
+    })
+})
+
+// send all turtles
+app.get('/turtles', (req, res) => {
+    res.json(turtles)
+})
+
+app.get('/turtles/:index', (req, res) => {
+    res.json(turtles[req.params.index])
+})
+
+// Create Route
+app.post('/turtles', (req, res) => {
+    turtles.push(req.body)
+    res.json(turtles)
 })
 
 
